823ca0db3594374d02e7e17e62cfb40dd78b00e7
filipjanusz <f.janusz@getprintbox.com>
2024-05-30 13:48:35 +0200

    add i18n

--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
-defineProps<{ msg: string }>()
+defineProps<{ msg: string }>();
 
-const count = ref(0)
+const count = ref(0);
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
-
+  <p>{{ $t('home.helloWorld') }}</p>
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
     <p>
--- client/src/main.ts
+++ client/src/main.ts
@@ -1,9 +1,18 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createI18n } from 'vue-i18n'
 import App from './App.vue'
+import pl from "./locales/pl.json"
+import en from "./locales/en.json"
 
 const pinia = createPinia()
+const i18n = createI18n({
+  locale: 'pl',
+  fallbackLocale: 'en',
+  messages: { pl, en }
+})
 const app = createApp(App)
 
 app.use(pinia)
+app.use(i18n)
 app.mount('#app')
